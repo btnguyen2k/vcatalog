8ab6c0e7098cebd08b1fc1d1c7ab86165b9d3303
btnguyen2k@gmail.com <btnguyen2k@gmail.com@f345d9bf-e6f8-e575-2749-99505cf5fbdb>
2012-07-13 18:14:15 +0000

    * Use MySQL as DB backend by default * Use system environment vairable VCATALOG_DBTYPE to configure db type

--- trunk/src/config/dzit-config-dao.php
+++ trunk/src/config/dzit-config-dao.php
@@ -5,8 +5,28 @@
 include_once 'dzit-config-dao-mysql.php';
 include_once 'dzit-config-dao-pgsql.php';
 
-global $DPHP_DAO_CONFIG_SITE;
-$DPHP_DAO_CONFIG_SITE = &$DPHP_DAO_CONFIG_PGSQL_SITE;
+$dbType = getenv('VCATALOG_DBTYPE');
+if ($dbType !== FALSE) {
+    if (strtoupper($dbType) == 'MYSQL') {
+        global $DPHP_DAO_CONFIG_SITE;
+        $DPHP_DAO_CONFIG_SITE = &$DPHP_DAO_CONFIG_MYSQL_SITE;
 
-global $DPHP_DAO_CONFIG;
-$DPHP_DAO_CONFIG = &$DPHP_DAO_CONFIG_PGSQL;
+        global $DPHP_DAO_CONFIG;
+        $DPHP_DAO_CONFIG = &$DPHP_DAO_CONFIG_MYSQL;
+    } elseif (strtoupper($dbType) == 'PGSQL' || strtoupper($dbType) == 'POSTGRES' || strtoupper($dbType) == 'POSTGRESQL') {
+        global $DPHP_DAO_CONFIG_SITE;
+        $DPHP_DAO_CONFIG_SITE = &$DPHP_DAO_CONFIG_PGSQL_SITE;
+
+        global $DPHP_DAO_CONFIG;
+        $DPHP_DAO_CONFIG = &$DPHP_DAO_CONFIG_PGSQL;
+    } else {
+        die("[$dbType] is not supported!");
+    }
+} else {
+    // MySQL by default
+    global $DPHP_DAO_CONFIG_SITE;
+    $DPHP_DAO_CONFIG_SITE = &$DPHP_DAO_CONFIG_MYSQL_SITE;
+
+    global $DPHP_DAO_CONFIG;
+    $DPHP_DAO_CONFIG = &$DPHP_DAO_CONFIG_MYSQL;
+}
